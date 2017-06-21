a8a10b9eac7c7e9e676690a5b46b5c579ef3dc2f
Terje Larsen <terlar@gmail.com>
2017-06-21 17:20:09 +0200

    lang: add configuration management tools to conf

--- elisp/lang-conf.el
+++ elisp/lang-conf.el
@@ -1,9 +1,13 @@
-;;; lang-conf.el --- Configuration formats
+;;; lang-conf.el --- Configuration formats/management
 
 ;;; Commentary:
 ;; Configuration, config, conf...
 
 ;;; Code:
+(require 'base-lib)
+
+;;;
+;; Packages
 (use-package nginx-mode)
 
 (use-package systemd
@@ -11,6 +15,25 @@
   (add-hook 'systemd-mode-hook
             #'(lambda ()
                 (run-hooks 'prog-mode-hook))))
+
+(use-package ansible
+  :after yaml-mode
+  :commands ansible
+  :init
+  (add-hook 'yaml-mode-hook #'ansible))
+
+(use-package ansible-doc :after ansible :commands ansible-doc)
+
+(use-package company-ansible
+  :after ansible
+  :commands company-ansible
+  :init
+  (with-eval-after-load "company"
+    (push-company-backends 'yaml-mode '(company-ansible))))
+
+(use-package puppet-mode
+  :mode (("\\.pp\\'"      . puppet-mode)
+         ("Puppetfile\\'" . puppet-mode)))
 
 (provide 'lang-conf)
 ;;; lang-conf.el ends here
