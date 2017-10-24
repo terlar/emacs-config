c6a679fd459d073c6c3ec2d2f1e561d11a4fc6b4
Terje Larsen <terlar@gmail.com>
2017-10-24 21:43:15 +0200

    lang: improve ebook by support by using nov.el

--- elisp/lang-ebook.el
+++ elisp/lang-ebook.el
@@ -4,11 +4,31 @@
 ;; Support for ePUB and PDF.
 
 ;;; Code:
-(use-package ereader :mode ("\\.epub$" . ereader-mode)
-  :commands ereader-mode
-  :config
-  (add-hooks-pair 'ereader-mode
-                  'page-break-lines-mode))
+(require 'base-vars)
+(require 'base-keybinds)
+
+(use-package nov :mode ("\\.epub$" . nov-mode)
+  :general
+  (:keymaps 'nov-mode-map :states 'normal
+            "]p"      'nov-next-document
+            "[p"      'nov-previous-document
+            "<up>"    'nov-scroll-down
+            "<down>"  'nov-scroll-up
+            "<left>"  'nov-previous-document
+            "<right>" 'nov-next-document)
+  :preface
+  (defvar nov-text-width)
+  (defvar cwm-centered-window-width)
+
+  (defun epub-setup ()
+    (setq nov-save-place-file (concat my-data-dir "nov-places")
+          nov-text-width most-positive-fixnum
+          cwm-centered-window-width 120)
+    (face-remap-add-relative 'variable-pitch :family "Noto Serif"
+                             :height 1.5))
+  :init
+  (add-hooks-pair 'nov-mode '(epub-setup
+                              centered-window-mode)))
 
 (use-package pdf-tools :mode ("\\.pdf$" . pdf-view-mode)
   :commands (pdf-view-mode pdf-tools-install))
