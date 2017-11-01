a8d2ce971648e918f211a581507e9323ef137ed7
Terje Larsen <terlar@gmail.com>
2017-11-01 15:28:29 +0100

    base: use indent-info

--- elisp/base-modeline.el
+++ elisp/base-modeline.el
@@ -72,6 +72,19 @@
 (use-package eldoc-eval :demand t
   :config (eldoc-in-minibuffer-mode +1))
 
+;; Display info about indentation current indentation settings
+(use-package indent-info :ensure nil :pin manual :demand t
+  :load-path "vendor/indent-info/"
+  :commands (indent-info-mode
+             global-indent-info-mode
+             indent-info-toggle-indent-mode
+             indent-info-cycle-tab-width-increase
+             indent-info-cycle-tab-width-decrease)
+  :config
+  ;; Setup indent info padding
+  (setq indent-info-prefix nil
+        indent-info-suffix " ")
+  (global-indent-info-mode +1))
 
 ;; Show icons instead of mode names
 (use-package mode-icons
