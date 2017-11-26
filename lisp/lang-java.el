314b895d0dcb15716c27d7f88333a842f3faeafb
Terje Larsen <terlar@gmail.com>
2017-11-26 02:49:23 +0100

    lang: add Java REPL

--- lisp/lang-java.el
+++ lisp/lang-java.el
@@ -25,6 +25,50 @@
   :config
   (smart-jump-register :modes 'java-mode))
 
+(req-package java-repl
+  :loader :path
+  :load-path my-site-lisp-dir
+  :commands run-java-repl
+  :init
+  (defalias 'java-repl 'run-java-repl)
+
+  (defun java-repl-eval-fn (stmt)
+    "Send STMT to Java REPL."
+    (let ((buffer-name "*java-repl*"))
+      (unless (get-buffer buffer-name)
+        (run-java-repl))
+
+      (with-current-buffer buffer-name
+        (insert stmt)
+        (comint-send-input))))
+
+  (defun java-repl-eval-region (begin end)
+    "Evaluate selected region from BEGIN to END."
+    (interactive "r")
+    (let ((stmt (buffer-substring begin end)))
+      (java-repl-eval-fn stmt)))
+
+  (defun java-repl-eval-line (&optional next-line)
+    "Evaluate current line."
+    (interactive "P")
+    (java-repl-eval-region (line-beginning-position) (line-end-position))
+    (when next-line
+      (call-interactively 'next-logical-line)))
+
+  (defun java-repl-eval ()
+    "Evaluate code in Java REPL."
+    (if (use-region-p)
+        (java-repl-eval-region (region-beginning) (region-end))
+      (java-repl-eval-line t)))
+
+  (set-repl-command 'java-mode #'java-repl)
+  (set-eval-command 'java-mode #'java-repl-eval)
+
+  (set-popup-buffer (rx bos "*java-repl*" eos))
+  (set-evil-state 'java-repl-mode 'insert)
+
+  (setq java-repl-file-path "/usr/bin/javarepl"))
+
 (req-package javadoc-lookup
   :commands javadoc-lookup
   :init
