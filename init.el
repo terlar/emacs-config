a7fe6158d6fc913be15193e761351a99f7d0be3b
Terje Larsen <terlar@gmail.com>
2018-04-25 21:47:26 +0200

    base: load all modules instead of explicit specification

--- init.el
+++ init.el
@@ -6,86 +6,23 @@
 ;;; Code:
 
 (eval-and-compile
-  (push (concat user-emacs-directory "lisp") load-path)
-  (push (concat user-emacs-directory "site-lisp") load-path))
+  (push (expand-file-name "lisp" user-emacs-directory) load-path)
+  (push (expand-file-name "site-lisp" user-emacs-directory) load-path))
+
+(defun load-directory-files-with-prefixes (dir types)
+  "Load Elisp files in DIR with TYPES prefix."
+  (let ((prefixes (mapconcat 'symbol-name types "\\|")))
+    (dolist (file (directory-files dir nil (concat "\\(" prefixes "\\)-[[:alnum:]-]*\\.el$")))
+      (load-file (expand-file-name file (file-name-as-directory dir))))))
 
 ;;;
 ;; Base
 
 ;; Calls (package-initialize)
 (require 'base)
-
-;;;
-;; Features
-
-(require 'feature-debug)
-(require 'feature-eval)
-(require 'feature-evil)
-(require 'feature-jump)
-(require 'feature-lsp)
-(require 'feature-preview)
-(require 'feature-snippets)
-(require 'feature-speed-reading)
-(require 'feature-spellcheck)
-(require 'feature-syntax-checker)
-(require 'feature-templates)
-(require 'feature-version-control)
-(require 'feature-workspaces)
-
-;;;
-;; Completion
-
-(require 'completion-company)      ; In-buffer completion
-(require 'completion-ivy)          ; General completion
-
-;;;
-;; Tools
-
-(require 'tool-container)          ; Container management
-(require 'tool-coverage)           ; Code coverage
-(require 'tool-dired)              ; Directories
-(require 'tool-docsets)            ; Docsets
-(require 'tool-exercism)           ; exercism.io
-(require 'tool-notes)              ; Note taking
-(require 'tool-pomodoro)           ; Pomodoro
-(require 'tool-recording)          ; Screencasting
-(require 'tool-rotate-text)        ; Toggle between different text
-(require 'tool-sidebar)            ; Sidebar
-(require 'tool-tramp)              ; Transparent Remote Access, Multiple Protocols
-
-;;;
-;; Language support
-
-(require 'lang-clojure)
-(require 'lang-conf)
-(require 'lang-crystal)
-(require 'lang-data)
-(require 'lang-ebook)
-(require 'lang-elixir)
-(require 'lang-elm)
-(require 'lang-emacs-lisp)
-(require 'lang-erlang)
-(require 'lang-go)
-(require 'lang-haskell)
-(require 'lang-java)
-(require 'lang-javascript)
-(require 'lang-lisp)
-(require 'lang-lua)
-(require 'lang-markdown)
-(require 'lang-ocaml)
-(require 'lang-opengl)
-(require 'lang-org)
-(require 'lang-pkgbuild)
-(require 'lang-python)
-(require 'lang-racket)
-(require 'lang-rest)
-(require 'lang-rst)
-(require 'lang-ruby)
-(require 'lang-rust)
-(require 'lang-scala)
-(require 'lang-shell)
-(require 'lang-uml)
-(require 'lang-web)
+(load-directory-files-with-prefixes
+ (expand-file-name "lisp" user-emacs-directory)
+ '(feature completion tool lang))
 
 (unless noninteractive
   (require 'bindings)
