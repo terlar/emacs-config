2b7e232ce0c15491f554c3c7cce4481a8b09c375
Terje Larsen <terlar@gmail.com>
2018-08-01 18:40:25 +0200

    feature: fix LSP definition and help lookup

--- lisp/feature/lsp.el
+++ lisp/feature/lsp.el
@@ -10,11 +10,16 @@
   (require 'base-package))
 
 (req-package eglot
+  :general
+  (:keymaps 'eglot-mode-map :states 'normal
+            "K" 'eglot-help-at-point)
   :hook
-  (js2-mode . eglot-ensure)
+  (js2-mode  . eglot-ensure)
   (ruby-mode . eglot-ensure)
-  (sh-mode . eglot-ensure)
+  (sh-mode   . eglot-ensure)
   :init
+  (with-eval-after-load 'smart-jump
+    (smart-jump-register :modes '(js2-mode ruby-mode sh-mode)))
   (set-popup-buffer (rx bos "*EGLOT" (one-or-more anything) "*" eos)))
 
 (provide 'feature-lsp)
