18f4971e8f2ffb50d3f8045a5dc472f2c9d80d78
Terje Larsen <terlar@gmail.com>
2018-09-22 21:25:45 +0200

    appearance: make text more visible again
    The upstream changes is a bit too vague for my taste.

--- lisp/local-theme.el
+++ lisp/local-theme.el
@@ -38,15 +38,18 @@
    `(vertical-border                         ((t (:background ,color-7 :foreground ,color-7))))
    `(mode-line                               ((t (:family ,+variable-pitch-font :background ,color-5 :foreground ,color-12 :box (:line-width 4 :color ,color-5)))))
    `(mode-line-inactive                      ((t (:family ,+variable-pitch-font :background ,color-4 :foreground ,color-9 :box (:line-width 4 :color ,color-4)))))
-   `(mode-line-buffer-id                     ((t (:foreground nil :weight bold))))
+   `(mode-line-buffer-id                     ((t (:foreground nil :bold t))))
    `(header-line-highlight                   ((t (:inherit mode-line-highlight))))
    `(line-number                             ((t (:inherit fixed-pitch))))
-   `(line-number-current-line                ((t (:inherit line-number :background ,color-5 :foreground ,theme-color-highlight :weight bold))))
-   `(hl-line                                 ((t (:background ,color-5))))
+   `(line-number-current-line                ((t (:inherit line-number :background ,color-5 :foreground ,theme-color-highlight :bold t))))
 
    `(error                                   ((t (:foreground ,theme-color-error))))
    `(success                                 ((t (:foreground ,theme-color-success))))
-   `(warning                                 ((t (:foreground ,theme-color-warning :weight bold))))
+   `(warning                                 ((t (:foreground ,theme-color-warning :bold t))))
+
+   `(font-lock-keyword-face                  ((t (:foreground ,color-13))))
+   `(font-lock-constant-face                 ((t (:foreground ,color-13 :weight normal))))
+   `(font-lock-type-face                     ((t (:foreground ,color-10))))
 
    ;; comint
    `(comint-highlight-prompt                 ((t (:height 1.0))))
@@ -62,6 +65,8 @@
    `(ediff-fine-diff-C                       ((t (:background "#4488BB"))))
    `(ediff-current-diff-Ancestor             ((t (:background "#DDF1F1"))))
    `(ediff-fine-diff-Ancestor                ((t (:background "#66CCCC"))))
+   ;; hl-line-mode
+   `(hl-line                                 ((t (:foreground nil))))
    ;; org-mode
    `(org-document-title                      ((t (:height 2.2 :bold nil))))
    `(org-level-1                             ((t (:height 1.6 :bold nil :italic t))))
@@ -97,8 +102,8 @@
    `(cargo-process--warning-face             ((t (:foreground ,theme-color-warning))))
    ;; company
    `(company-tooltip                         ((t (:family ,+variable-pitch-font))))
-   `(company-tooltip-search                  ((t (:weight bold))))
-   `(company-tooltip-search-selection        ((t (:weight bold))))
+   `(company-tooltip-search                  ((t (:bold t))))
+   `(company-tooltip-search-selection        ((t (:bold t))))
    ;; eros
    `(eros-result-overlay-face                ((t (:background ,color-5 :foreground ,color-9 :box nil))))
    ;; indent-guide
@@ -142,7 +147,9 @@
    ;; org-tree-slide
    `(org-tree-slide-header-overlay-face      ((t (:inherit header-line :background nil))))
    ;; perspeen
-   `(perspeen-selected-face                  ((t (:weight bold))))
+   `(perspeen-selected-face                  ((t (:bold t))))
+   ;; rainbow-delimiters
+   `(rainbow-delimiters-unmatched-face       ((t (:foreground ,theme-color-error :bold t))))
    ;; rst
    `(rst-level-1                             ((t (:weight normal :italic t))))
    `(rst-level-2                             ((t (:weight normal :italic t))))
@@ -155,17 +162,17 @@
    `(shm-current-face                        ((t (:inherit hl-line))))
    `(shm-quarantine-face                     ((t (:background "#EECCCC"))))
    ;; smartparens
-   `(sp-show-pair-match-face                 ((t (:background ,color-3 :foreground ,color-11 :weight bold))))
-   `(sp-show-pair-mismatch-face              ((t (:background ,color-7 :foreground ,color-1 :weight bold))))
+   `(sp-show-pair-match-face                 ((t (:foreground ,color-11 :bold t))))
+   `(sp-show-pair-mismatch-face              ((t (:foreground ,theme-color-error :bold t))))
    ;; spray
    `(spray-base-face                         ((t (:foreground ,color-10 :family "Noto Serif" :weight normal :underline nil))))
    `(spray-accent-face                       ((t (:foreground ,theme-color-error :underline (:color ,color-10) :overline ,color-10))))
    ;; stripe-buffer
    `(stripe-highlight                        ((t (:background ,color-5))))
    ;; web-mode
-   `(web-mode-current-element-highlight-face ((t (:background nil :foreground nil :weight bold))))
+   `(web-mode-current-element-highlight-face ((t (:background nil :foreground nil :bold t))))
    ;; which-key
-   `(which-key-local-map-description-face    ((t (:weight bold))))
+   `(which-key-local-map-description-face    ((t (:bold t))))
    ;; quick-peek
    `(quick-peek-border-face                  ((t (:background ,color-5 :height 0.1 :box (:line-width 1 :color ,color-7)))))
    `(quick-peek-padding-face                 ((t (:height 0.1)))))
