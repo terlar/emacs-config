0fd07300b24cb93c9f274154389abd82671835e7
Terje Larsen <terlar@gmail.com>
2020-07-01 08:45:10 +0200

    flake: update inputs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Emacs config of Terje";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.03";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
 
     emacs-overlay.url = "github:nix-community/emacs-overlay";
