0b9d49706be71df4218dc28b4145d58d97803725
Terje Larsen <terlar@gmail.com>
2020-08-24 12:13:26 +0200

    templates/*.el: update

--- templates/09:.*.el
+++ templates/09:.*.el
@@ -1,20 +1,22 @@
-;;; `(file-name-nondirectory buffer-file-name)` --- ${1:summary} -*- coding: utf-8; lexical-binding: t; -*-
+;;; `(file-name-nondirectory buffer-file-name)` --- ${1:summary} -*- coding: utf-8; lexical-binding: t -*-
 
 ;; Copyright (C) `(format-time-string "%Y")` `user-full-name`
 ;; All rights reserved.
 
 ;; Author: `user-full-name` <`user-mail-address`>
-;; Keywords: ${2:keywords}
+;; URL: ${2:url}
+;; Keywords: ${3:keywords}
 ;; Version: 0.1
+;; Package-Requires: ((emacs "24.3"))
 
 ;; This file is NOT part of GNU Emacs.
 
-;; indent-info is free software: you can redistribute it and/or modify
+;; `(file-name-sans-extension (file-name-nondirectory buffer-file-name))` is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
 
-;; indent-info is distributed in the hope that it will be useful,
+;; `(file-name-sans-extension (file-name-nondirectory buffer-file-name))` is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
@@ -24,7 +26,7 @@
 
 ;;; Commentary:
 
-;; ${3:description}
+;; ${4:description}
 
 ;;; Code:
 
