7d36f1d0e03b29cb1e78520dc5b5a5ba1a73f998
Terje Larsen <terlar@gmail.com>
2020-12-30 00:26:31 +0100

    flake: move from PGTK GCC to PGTK

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
         in (emacs-overlay.overlay final prev) // rec {
           emacsEnv = final.emacsWithPackagesFromUsePackage {
             config = ./init.org;
-            package = final.emacsPgtkGcc;
+            package = final.emacsPgtk;
 
             extraEmacsPackages = epkgs: [ epkgs.mini-frame ];
 
