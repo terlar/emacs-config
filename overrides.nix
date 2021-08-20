498366961fc2e3bdac9b48930dfeb83b5184589f
Terje Larsen <terlar@gmail.com>
2021-08-20 22:56:04 +0200

    overrides (puni): 20210820.103 -> 20210821.10

--- overrides.nix
+++ overrides.nix
@@ -179,13 +179,13 @@ epkgs // {
 
   puni = trivialBuild rec {
     pname = "puni";
-    version = "20210820.103";
+    version = "20210821.10";
     src = fetchFromGitHub {
       owner = "AmaiKinono";
       repo = "puni";
-      rev = "9eafbb3ab6fc2f4f3a45982843072122b7ec9832";
-      sha256 = "0ybkljsk8wmz6myd71n4457d3zd4164vd3wf3j9nqfvn76shwq9x";
-      # date = 2021-08-20T01:03:05+08:00;
+      rev = "29ae364e9028460accd32b54d80d33a6b411602b";
+      sha256 = "0cng3xp3x30y80c0mmnjjsvrdhp66gkgqqkk5c38a3ybi9m3xj9c";
+      # date = 2021-08-21T00:10:26+08:00;
     };
   };
 
