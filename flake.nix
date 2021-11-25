b7ecde5921fbabcc1ceb35b3971f351f6465d04c
Terje Larsen <terlar@gmail.com>
2021-11-25 22:57:48 +0100

    flake: only build x86_64-linux and use nix 2.4

--- flake.nix
+++ flake.nix
@@ -20,14 +20,8 @@
     let
       inherit (nixpkgs) lib;
 
-      supportedSystems = [
-        "aarch64-linux"
-        "aarch64-darwin"
-        "i686-linux"
-        "x86_64-darwin"
-        "x86_64-linux"
-      ];
-      forAllSystems = f: lib.genAttrs supportedSystems (system: f system);
+      systems = [ "x86_64-linux" ];
+      forAllSystems = f: lib.genAttrs systems (system: f system);
       nixpkgsFor = forAllSystems (system: import nixpkgs {
         inherit system;
         overlays = [ self.overlay ];
@@ -132,7 +126,7 @@
             nativeBuildInputs = with pkgs; [
               gdb
               git
-              nixUnstable
+              nix_2_4
               nixpkgs-fmt
 
               devEmacsConfig
