b3840fa56a7812c2c3cc56f9d5c27a93405bc940
Terje Larsen <terlar@gmail.com>
2023-11-17 00:03:19 +0100

    init (testing): evaluate corfu-candidate-overlay (#254)

--- lock/flake.nix
+++ lock/flake.nix
@@ -193,6 +193,12 @@
       repo = "corfu";
       type = "github";
     };
+    corfu-candidate-overlay = {
+      flake = false;
+      owner = "terlar";
+      repo = "corfu-candidate-overlay";
+      type = "github";
+    };
     cov = {
       flake = false;
       owner = "AdamNiederer";
