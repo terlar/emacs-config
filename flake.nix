f24c5239dd7831f4570ac8f890af7bceb3eb400a
Terje Larsen <terlar@gmail.com>
2024-04-02 13:20:30 +0200

    chore(flake): update flake.lock

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     emacs-overlay = {
       url = "github:nix-community/emacs-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
