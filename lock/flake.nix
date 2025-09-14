9ee2614213b9203d974f85c4a6fbe060221436ad
Terje Larsen <terlar@gmail.com>
2025-09-14 18:15:28 +0200

    feat: remove copilot in favor of gptel with gptel-autocomplete

--- lock/flake.nix
+++ lock/flake.nix
@@ -115,12 +115,6 @@
       repo = "consult-git-log-grep";
       type = "github";
     };
-    copilot = {
-      flake = false;
-      owner = "copilot-emacs";
-      repo = "copilot.el";
-      type = "github";
-    };
     corfu = {
       flake = false;
       owner = "minad";
@@ -233,12 +227,6 @@
       flake = false;
       owner = "Fanael";
       repo = "edit-indirect";
-      type = "github";
-    };
-    editorconfig = {
-      flake = false;
-      owner = "editorconfig";
-      repo = "editorconfig-emacs";
       type = "github";
     };
     elisp-refs = {
